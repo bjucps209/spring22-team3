8d64ed224929a3dad1d5efa9613461b04f4bacca
Edward Taylor <etayl261@students.bju.edu>
2022-04-05 14:04:06 -0400

    Classes for core gameplay, no code yet

--- microwaveDungeon/src/MainWindow.java
+++ microwaveDungeon/src/MainWindow.java
@@ -2,15 +2,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Pane;
 
 
-public class MainWindow {
+public class MainWindow { 
 
     @FXML
-    void onGreetClicked(ActionEvent event) {
-        var alert = new Alert(AlertType.INFORMATION, "Hello, world!");
-        alert.setHeaderText(null);
-        alert.show();
+    Pane pane;
 
-    }
+    
 }
