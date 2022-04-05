d2e8763731bf6ceda58a71a1e3cc1bf546acf51e
Ethan Wood <ewood351@students.bju.edu>
2022-04-05 17:06:09 -0400

    Added Pause Window with skeleton code

---
+++ microwaveDungeon/src/PauseMenu.java
@@ -0,0 +1,28 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Pane;
+
+public class PauseMenu {
+    
+    // TODO: Resumes the game
+    @FXML
+    void onResumeClicked(ActionEvent e) {
+    }
+
+    // TODO: Saves the game
+    @FXML
+    void onSaveClicked(ActionEvent e){
+    }
+
+    // TODO: Closes game and opens main menu/restarts game
+    @FXML
+    void onQuitMainMenuClicked(ActionEvent e) {
+    }
+
+    // TODO: Closes entire game
+    @FXML
+    void onQuitDesktopClicked(ActionEvent e) {
+    }
+}
