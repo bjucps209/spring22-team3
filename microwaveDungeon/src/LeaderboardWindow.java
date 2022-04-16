abd1eefa4a039bb79d50af76815fb270f33722f1
dhaye039 <dhaye039@students.bju.edu>
2022-04-16 19:58:06 -0400

    updated scoring

--- microwaveDungeon/src/LeaderboardWindow.java
+++ microwaveDungeon/src/LeaderboardWindow.java
@@ -14,7 +14,7 @@ public class LeaderboardWindow {
     @FXML
     Label score1, score2, score3, score4, score5, score6, score7, score8, score9, score10; // tenetive naming system, seems clunky so this may get simplified
 
-    void initialize() {
+    void initialize() throws IOException {
         ScoreList List = new ScoreList();
         var scoreList = List.getScoreList();
         // make sure to check to see if the list is right, if it isnt, rewrite the data from the file onto the scoreList
--- microwaveDungeon/src/model/ScoreList.java
+++ microwaveDungeon/src/model/ScoreList.java
@@ -1,54 +1,61 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+
 
 public class ScoreList {
 
-    public ArrayList<Scores> scoreList;
-
+    private ArrayList<Scores> scoreList;
     static final String FILENAME = "scores.txt";
 
     public ScoreList() {
         scoreList = new ArrayList<Scores>();
     }
 
-    // // gets the scores from the .txt file and adds it to the scoreList variable 
-    // public void getScores() {
-    //     throw new RuntimeException("Method not implemented");
-    //     // get scores from txt file and save them as 
-    //     // Score objects with loadData().
-
-    //     // scoreList.add(Score_Object);
-    //     // sort();
-    // }
-
     // Writes the data from the list to the txt file, is used after getScores()
-    public void saveData() {
-        throw new RuntimeException("Method not implemented");
-        // for (Scores s : scoreList) {
-        //    write(s);
-        // }
+    public void saveData() throws IOException {
+        try (var writer = new PrintWriter(new FileWriter(FILENAME))) {
+            for (Scores obj : this.scoreList) {
+                String name = obj.getName();
+                int score = obj.getScore();
+                writer.println(name + "," + score);
+            }
+        }
     }
 
-    // Reads the data from the file and places them into the scoreList variable 
+    // Reads the data from the file and places them into the scoreList variable
     // then sorts the list from highest score to lowest score.
-    public void loadData() {
-        throw new RuntimeException("Method not implemented");
-        // get scores from txt file and save them as 
-        // Score objects with loadData().
-
-        // scoreList.add(Score_Object);
-        // sort();
-
+    public void loadData() throws IOException {
+        if (Files.exists(Paths.get(FILENAME))) {
+            try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
+                String str;
+                while ((str = br.readLine()) != null) {
+                    String[] strArr = str.split(",");
+                    String name = strArr[0];
+                    String score = strArr[1];
+                    Scores scoreObj = new Scores(name, Integer.parseInt(score));
+                    this.scoreList.add(scoreObj);
+                }
+            }
+        }
+        sortList();
     }
 
     // Sorts the array list from highest score to lowest
     public ArrayList<Scores> sortList() {
-        throw new RuntimeException("Method not implemented");
+        scoreList.sort((o1, o2) -> o2.getScore() - o1.getScore());
+        return scoreList;
     }
 
     // adds a score to the list
-    public void add(Scores score) {
+    public void addScore(Scores score) {
         this.scoreList.add(score);
     }
 
@@ -57,8 +64,9 @@ public class ScoreList {
     }
 
     // getter method
-    public ArrayList<Scores> getScoreList() {
-        return scoreList;
+    public ArrayList<Scores> getScoreList() throws IOException {
+        loadData();
+        return this.scoreList;
     }
 
 }
