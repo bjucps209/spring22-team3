601d0e48a9ceb85a0c0df984d8b7bb864e528449
Eddi Taylor <etayl261@students.bju.edu>
2022-04-21 19:30:22 -0400

    enemy pursuit efficiency update

--- microwaveDungeon/src/model/enemy.java
+++ microwaveDungeon/src/model/enemy.java
@@ -16,35 +16,14 @@ public class enemy extends entity {
 
     public void updatePosition(Double playerX, Double playerY) {
 
-        double direction2 = 0;
-
-        // if (Xcoord > 710) {
-        //     Xcoord -= 710;
-        // } else if (Ycoord > 550) {
-        //     Ycoord -= 550;
-        // } else if (Xcoord < 0) {
-        //     Xcoord += 710;
-        // } else if (Ycoord < 0) {
-        //     Ycoord += 550;
-        // }
-        // ++updateCount;
-        // Random rand = new Random();
-        // if (updateCount % 5 == 0) {
-        //     direction = rand.nextInt(360);
-        // }
-
         if (playerX < Xcoord && playerY < Ycoord){
             direction = Math.atan((playerY - Ycoord)/(playerX - Xcoord)) *(180 / Math.PI) + 180;
-            direction2 = Math.atan((playerY - Ycoord)/(playerX - Xcoord)) *(180 / Math.PI) + 180;
         } else if (playerX < Xcoord && playerY > Ycoord){
             direction = Math.atan((playerY - Ycoord)/(playerX - Xcoord)) *(180 / Math.PI) + 180;
-            direction2 = Math.atan((playerY - Ycoord)/(playerX - Xcoord)) *(180 / Math.PI) + 180;
         } else if (playerX > Xcoord && playerY < Ycoord){
             direction = Math.atan((playerY - Ycoord)/(playerX - Xcoord)) *(180 / Math.PI);
-            direction2 = Math.atan((playerY - Ycoord)/(playerX - Xcoord)) *(180 / Math.PI);
         } else if (playerX > Xcoord && playerY > Ycoord){
             direction = Math.atan((playerY - Ycoord)/(playerX - Xcoord)) *(180 / Math.PI);
-            direction2 = Math.atan((playerY - Ycoord)/(playerX - Xcoord)) *(180 / Math.PI);
         }
          
 
@@ -52,7 +31,7 @@ public class enemy extends entity {
 
         
         Xcoord += speed * Math.cos(direction * Math.PI / 180);
-        Ycoord += speed * Math.sin(direction2 * Math.PI / 180);
+        Ycoord += speed * Math.sin(direction * Math.PI / 180);
         
 
     }
