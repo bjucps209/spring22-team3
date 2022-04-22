473ec7ee0cd461e13e0124ffc8ab209f7f02b3d6
dhaye039 <dhaye039@students.bju.edu>
2022-04-22 17:48:59 -0400

    Added a Help screen

---
+++ microwaveDungeon/src/HelpWindow.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class HelpWindow {
+
+    @FXML
+    void onBackClicked(ActionEvent event) throws IOException{
+        var loader = new FXMLLoader(getClass().getResource("TitleWindow.fxml"));
+        var scene = new Scene(loader.load());
+
+        var stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+        stage.setTitle("MicrowaveDungeon");
+
+        Stage titleStage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        titleStage.close();
+    }
+    
+}
